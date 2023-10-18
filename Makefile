275eb7da161f1f459362f752731b66f46446dbd9
Sudarshan12D <sudarshan.naicker1@ucalgary.ca>
2023-10-18 08:21:09 +0000

    RR done

--- Makefile
+++ Makefile
@@ -10,20 +10,20 @@ clean:
 
 .PHONY: test, test2, test3, test4, test5, test6
 test: scheduler.out
-	./scheduler.out 0 STCF example_stcf.in 10
+	./scheduler.out 0 STCF example_stcf.in 5
 
 test2: scheduler.out
 	./scheduler.out 1 STCF example_stcf.in 5
 
 test3: scheduler.out
-	./scheduler.out 0 RR example_rr.in 5
+	./scheduler.out 0 RR example_rr.in 2
 
 test4: scheduler.out
-	./scheduler.out 1 RR example_rr.in 5
+	./scheduler.out 1 RR example_rr.in 2
 
 test5: scheduler.out
-	./scheduler.out 0 LT example_lt.in 5
+	./scheduler.out 0 LT example_lt.in 10
 
 test6: scheduler.out
-	./scheduler.out 1 LT example_lt.in 5
+	./scheduler.out 1 LT example_lt.in 10
 
--- scheduler.c
+++ scheduler.c
@@ -5,6 +5,9 @@
 #include <string.h>
 #include <limits.h>
 
+
+
+
 // TODO: Add more fields to this struct
 struct job
 {
@@ -13,8 +16,14 @@ struct job
   int length;
   int tickets;
   struct job *next;
+  
+
   int startTime;
   int endTime;
+
+  int originalLength; // New field
+  int executed;
+
 };
 
 /*** Globals ***/
@@ -25,83 +34,7 @@ struct job *head = NULL;
 
 /*** Globals End ***/
 
-/*Function to sort linked list by arrival time*/
-void sort_by_arrival(struct job **head_ptr)
-{
-  struct job *current, *next;
-  int swapped;
 
-  do
-  {
-    swapped = 0;
-    current = *head_ptr;
-
-    while (current->next != NULL)
-    {
-      next = current->next;
-      if (current->arrival > next->arrival)
-      {
-        // Swap data
-        int temp_id = current->id;
-        int temp_arrival = current->arrival;
-        int temp_length = current->length;
-        int temp_tickets = current->tickets;
-
-        current->id = next->id;
-        current->arrival = next->arrival;
-        current->length = next->length;
-        current->tickets = next->tickets;
-
-        next->id = temp_id;
-        next->arrival = temp_arrival;
-        next->length = temp_length;
-        next->tickets = temp_tickets;
-
-        swapped = 1;
-      }
-      current = current->next;
-    }
-  } while (swapped);
-}
-
-/*Function to sort the linked list based on length*/
-void sort_by_length(struct job **head_ptr)
-{
-  struct job *current, *next;
-  int swapped;
-
-  do
-  {
-    swapped = 0;
-    current = *head_ptr;
-
-    while (current->next != NULL)
-    {
-      next = current->next;
-      if (current->length > next->length)
-      {
-        // Swap data
-        int temp_id = current->id;
-        int temp_arrival = current->arrival;
-        int temp_length = current->length;
-        int temp_tickets = current->tickets;
-
-        current->id = next->id;
-        current->arrival = next->arrival;
-        current->length = next->length;
-        current->tickets = next->tickets;
-
-        next->id = temp_id;
-        next->arrival = temp_arrival;
-        next->length = temp_length;
-        next->tickets = temp_tickets;
-
-        swapped = 1;
-      }
-      current = current->next;
-    }
-  } while (swapped);
-}
 
 /*Function to append a new job to the list*/
 void append(int id, int arrival, int length, int tickets)
@@ -114,6 +47,12 @@ void append(int id, int arrival, int length, int tickets)
   tmp->length = length;
   tmp->arrival = arrival;
   tmp->tickets = tickets;
+
+  tmp->startTime = -1;
+  //tmp->endTime = -1;
+  tmp->executed = 0;
+  tmp->originalLength = length; 
+
 
   // the new job is the last job
   tmp->next = NULL;
@@ -138,6 +77,7 @@ void append(int id, int arrival, int length, int tickets)
   return;
 }
 
+
 /*Function to read in the workload file and create job list*/
 void read_workload_file(char *filename)
 {
@@ -161,6 +101,10 @@ void read_workload_file(char *filename)
     length = strtok(NULL, ",\n");
     tickets += 100;
 
+
+    
+
+
     // Make sure neither arrival nor length are null.
     assert(arrival != NULL && length != NULL);
 
@@ -175,79 +119,163 @@ void read_workload_file(char *filename)
   return;
 }
 
+// New functions
+
+
+void sort_by_arrival() {
+    struct job *current, *next;
+    int swapped;
+
+    do {
+        swapped = 0;
+        current = head;
+
+        while (current != NULL && current->next != NULL) {
+            next = current->next;
+            if (current->arrival > next->arrival) {
+                // Swap the jobs
+                int temp_id = current->id;
+                int temp_arrival = current->arrival;
+                int temp_length = current->length;
+
+                current->id = next->id;
+                current->arrival = next->arrival;
+                current->length = next->length;
+
+                next->id = temp_id;
+                next->arrival = temp_arrival;
+                next->length = temp_length;
+
+                swapped = 1;  // mark that a swap occurred
+            }
+            current = current->next;
+        }
+    } while (swapped);
+}
+
+
 void policy_STCF(struct job *head, int slice)
 {
-  int current_time = 0;
-
-  printf("Execution trace with STCF:\n");
-
-  while (head != NULL)
-  {
-    struct job *current = head;
-    struct job *shortest = NULL;
-
-    // Find the job with the shortest remaining duration
-    while (current != NULL)
-    {
-      if (current->arrival <= current_time)
-      {
-        if (shortest == NULL || current->length < shortest->length)
-        {
-          shortest = current;
-        }
-      }
-      current = current->next;
-    }
-
-    if (shortest == NULL)
-    {
-      // No jobs are ready to run at this time
-      current_time++;
-    }
-    else
-    {
-      // Run JL for S ticks
-      int run_time = (shortest->length < slice) ? shortest->length : slice;
-
-      printf("t=%d: [Job %d] arrived at [%d], ran for: [%d]\n",
-             current_time, shortest->id, shortest->arrival, run_time);
-
-      shortest->length -= run_time;
-      current_time += run_time;
-
-      // Check if the job is complete
-      if (shortest->length == 0)
-      {
-        struct job *temp = shortest;
-        head = shortest->next;
-        free(temp);
-      }
-    }
-  }
-
-  printf("End of execution with STCF.\n");
+  
+ 
 }
 
 void analyze_STCF(struct job *head)
 {
-  // TODO: Fill this in
-
-  return;
+  
 }
 
-void policy_RR(struct job *head, int slice)
-{
-  // TODO: Fill this in
 
+void policy_RR(struct job *head, int slice) {
+
+
+  sort_by_arrival(); 
+  int currentTime = 0;
+  int total_jobs = 0; //finding total jobs
+  int job_executed = 0;
+  int jobs_ran_this_round;
+
+  struct job *job_counter = head;
+  while (job_counter) {
+    total_jobs++;
+    job_counter = job_counter->next;
+  }
+  
+  printf("\nStart of execution with RR.\n\n");
+
+  struct job *current = head; //current pointer points to head
+  while (job_executed < total_jobs)  // Keep looping till no jobs are left to be executed
+  {
+    current = head;
+    jobs_ran_this_round = 0;
+    
+    while (current != NULL)
+    {
+      if (current->arrival <= currentTime && current->length != 0)
+      {
+        
+        jobs_ran_this_round++;
+        int time_ran = 0;
+        if(current->length > slice){
+          time_ran = slice;   
+          current->length -= time_ran;
+        }
+        else{
+          time_ran = current->length;
+          current->length = 0;
+          job_executed++;
+        }
+
+        printf("job exec%d", job_executed);
+        printf("  t=%d: [Job %d] arrived at [%d], ran for: [%d]\n", currentTime, current->id, current->arrival, time_ran);
+        
+        if (current->startTime == -1) {
+          current->startTime = currentTime;
+        }
+        
+        currentTime += time_ran;
+
+        if (current->length == 0) {
+            current->endTime = currentTime;
+        }
+        
+      }
+      current = current->next;
+    }
+
+    if (jobs_ran_this_round == 0) { // if no jobs ran in this round, advance the clock
+      struct job *temp = head;
+      while (temp != NULL && temp->length == 0) { // find the next job that hasn't started yet
+        temp = temp->next;
+      }
+      if (temp != NULL) { // if found, set currentTime to its arrival time
+        currentTime = temp->arrival;
+      }
+    }
+  }
+  printf("\nEnd of execution with RR.\n");
   return;
+
 }
 
 void analyze_RR(struct job *head)
 {
-  // TODO: Fill this in
+  struct job *current = head;
+    double total_response = 0, total_turnaround = 0, total_wait = 0;
+    int count = 0;
 
-  return;
+    printf("\nBegin analyzing RR:\n");
+
+    while (current != NULL)
+    {
+        // Response time: time when job first gets the CPU - arrival time
+        int response_time = current->startTime - current->arrival;
+
+        // Turnaround time: completion time - arrival time
+        int turnaround_time = current->endTime - current->arrival;
+
+        // Wait time: Turnaround time - total execution time
+        int wait_time = turnaround_time - current->originalLength;
+
+        printf("Job %d -- Response time: %d  Turnaround: %d  Wait: %d\n",
+               current->id, response_time, turnaround_time, wait_time);
+
+        total_response += response_time;
+        total_turnaround += turnaround_time;
+        total_wait += wait_time;
+        count++;
+        current = current->next;
+    }
+
+    // Compute average metrics
+    double avg_response = total_response / count;
+    double avg_turnaround = total_turnaround / count;
+    double avg_wait = total_wait / count;
+
+    printf("Average -- Response: %.2f  Turnaround %.2f  Wait %.2f\n", avg_response, avg_turnaround, avg_wait);
+    printf("\nEnd analyzing RR.\n");
 }
+
 
 void policy_LT(struct job *head, int slice)
 {
@@ -281,27 +309,30 @@ int main(int argc, char **argv)
   // Note: we use a global variable to point to
   // the start of a linked-list of jobs, i.e., the job list
   read_workload_file(workload);
+  
 
   if (strcmp(policy, "STCF") == 0)
   {
     policy_STCF(head, slice);
     if (analysis)
     {
-      printf("Begin analyzing STCF:\n");
+      printf("Begin analyzing STCF:\n\n");
       analyze_STCF(head);
-      printf("End analyzing STCF.\n");
+      printf("\nEnd analyzing STCF.\n");
     }
 
     exit(EXIT_SUCCESS);
   }
+
+
   if (strcmp(policy, "RR") == 0)
   {
     policy_RR(head, slice);
     if (analysis)
     {
-      printf("Begin analyzing RR:\n");
+      printf("Begin analyzing RR:\n\n");
       analyze_RR(head);
-      printf("End analyzing RR.\n");
+      printf("\nEnd analyzing RR.\n");
     }
 
     exit(EXIT_SUCCESS);
@@ -311,9 +342,9 @@ int main(int argc, char **argv)
     policy_LT(head, slice);
     if (analysis)
     {
-      printf("Begin analyzing STCF:\n");
+      printf("Begin analyzing STCF:\n\n");
       analyze_LT(head);
-      printf("End analyzing STCF.\n");
+      printf("\nEnd analyzing STCF.\n");
     }
 
     exit(EXIT_SUCCESS);
